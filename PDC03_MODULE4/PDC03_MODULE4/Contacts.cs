898d035ba35ae62311725c506048d4ac0d18f25e
Snoopy <99853131+basiliosteph@users.noreply.github.com>
2023-05-12 12:12:16 +0800

    Module 04 Activities 02 upload

---
+++ PDC03_MODULE4/PDC03_MODULE4/Contacts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace PDC03_MODULE4
+{
+
+    public class Contacts
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Occupation { get; set; }
+
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
